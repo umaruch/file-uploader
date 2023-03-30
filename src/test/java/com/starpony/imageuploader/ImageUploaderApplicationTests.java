0c652d6be664f5256c96f58abb187de380729c62
umaruch <60070943+umaruch@users.noreply.github.com>
2023-03-30 21:50:14 +0500

    Базовый каркас приложения

---
+++ src/test/java/com/starpony/imageuploader/ImageUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starpony.imageuploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
