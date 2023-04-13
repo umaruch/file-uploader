6bfa83fb3d0c573106240a5c696847af66bf62d7
umaruch <130343724+starponycorp@users.noreply.github.com>
2023-04-13 15:57:49 +0500

    Добавлен TM ImageIO в зависимости

--- src/main/java/com/starpony/imageuploader/images/ImageUtils.java
+++ src/main/java/com/starpony/imageuploader/images/ImageUtils.java
@@ -3,24 +3,20 @@ package com.starpony.imageuploader.images;
 import com.starpony.imageuploader.images.errors.ImagesException;
 import com.starpony.imageuploader.images.models.ImageFormat;
 
-import net.coobird.thumbnailator.Thumbnails;
-import net.coobird.thumbnailator.geometry.Positions;
-import net.coobird.thumbnailator.tasks.UnsupportedFormatException;
-
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.*;
 
 
 public class ImageUtils {
     public static ByteArrayInputStream resize(InputStream stream, ImageFormat format) throws ImagesException {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         try {
-            Thumbnails.of(stream).crop(Positions.CENTER).size(format.getWidth(), format.getHeight())
-                    .keepAspectRatio(true).outputFormat(format.getType()).toOutputStream(outputStream);
-        } catch (UnsupportedFormatException ex) {
-            throw new ImagesException("The image must be in format .png, .jpg or jpeg");
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
+            BufferedImage image =
+        //
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        return new ByteArrayInputStream(outputStream.toByteArray());
     }
 }
